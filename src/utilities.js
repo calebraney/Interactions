f78fb314308880219c3c22e46fc2d90c727d1980
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2026-01-27 14:05:33 -0800

    more fixes

--- src/utilities.js
+++ src/utilities.js
@@ -29,10 +29,8 @@ export const attr = function (defaultVal, attrVal) {
   //get the type of the default
   const defaultValType = typeof defaultVal;
   if (typeof attrVal !== 'string' || attrVal.trim() === '') return defaultVal;
-  if (attrVal === 'true' && defaultValType === 'boolean') return true;
-  if (attrVal === 'True' && defaultValType === 'boolean') return true;
-  if (attrVal === 'false' && defaultValType === 'boolean') return false;
-  if (attrVal === 'False' && defaultValType === 'boolean') return false;
+  if (attrVal?.toLowerCase() === 'true' && defaultValType === 'boolean') return true;
+  if (attrVal?.toLowerCase() === 'false' && defaultValType === 'boolean') return false;
   if (isNaN(attrVal) && defaultValType === 'string') return attrVal;
   if (!isNaN(attrVal) && defaultValType === 'number') return +attrVal;
   return defaultVal;
