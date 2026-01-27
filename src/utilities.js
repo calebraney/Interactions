ffbee814d091da2991da6f9d7adb413d1dfecc76
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2026-01-27 13:32:07 -0800

    updated attributes script

--- src/utilities.js
+++ src/utilities.js
@@ -30,7 +30,9 @@ export const attr = function (defaultVal, attrVal) {
   const defaultValType = typeof defaultVal;
   if (typeof attrVal !== 'string' || attrVal.trim() === '') return defaultVal;
   if (attrVal === 'true' && defaultValType === 'boolean') return true;
+  if (attrVal === 'True' && defaultValType === 'boolean') return true;
   if (attrVal === 'false' && defaultValType === 'boolean') return false;
+  if (attrVal === 'False' && defaultValType === 'boolean') return false;
   if (isNaN(attrVal) && defaultValType === 'string') return attrVal;
   if (!isNaN(attrVal) && defaultValType === 'number') return +attrVal;
   return defaultVal;
